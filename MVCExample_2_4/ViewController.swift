34161b00baefb1d0ef7ccbd2b1d64598f859767e
laurayesayan <laurayesayan@gmail.com>
2020-03-03 23:01:17 +0300

    MVC implementation of categories loader and to do list tasks.

--- MVCExample_2_4/ViewController.swift
+++ MVCExample_2_4/ViewController.swift
@@ -8,13 +8,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Foundation
 
+import Bond
+
+import Alamofire
+
+import Kingfisher
+
+class ViewController: UIViewController {
+    @IBOutlet weak var categoriesTableView: UITableView!
+    let categoriesViewModel = CategoriesViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bindCategoriesToTableView()
+        categoriesViewModel.loadCategories(url: "http://blackstarshop.ru/index.php?route=api/v1/categories")
+    }
+    
+    func bindCategoriesToTableView() {
+        categoriesViewModel.categoriesList.bind(to: categoriesTableView) { (dataSource, indexPath, tableView) -> UITableViewCell in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CategoriesCell") as! CategoriesTableViewCell
+            
+            cell.titleLabel.text = dataSource[indexPath.row].name
+            
+            var url = URL(string: "http://blackstarshop.ru/\(dataSource[indexPath.row].iconImage)")
+            
+            // При отсутствии картинки вставялем дефолтную
+            if dataSource[indexPath.row].iconImage.isEmpty {
+                url = URL(string: "http://blackstarshop.ru/image/catalog/style/modile/acc_cat.png")
+            }
+            
+            cell.iconImageView.kf.setImage(with: url)
+            
+            return cell
+        }.dispose(in: reactive.bag)
     }
 
 
 }
 
+class CategoriesTableViewCell: UITableViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
+
+class CategoriesViewModel {
+    public var categoriesList = MutableObservableArray<Categories>([])
+    
+    func loadCategories(url: String) {
+        AF.request(url).responseJSON {
+            response in
+            if let json = response.value,
+                let jsonDict = json as? NSDictionary {
+                for (_, data) in jsonDict {
+                    if let categories = Categories(data: data as! NSDictionary) {
+                        self.categoriesList.append(categories)
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+struct Categories {
+    let name: String
+    let iconImage: String
+    
+    init?(data: NSDictionary) {
+        guard let name = data["name"] as? String,
+            let iconImage = data["iconImage"] as? String else {
+                return nil
+        }
+        
+        self.name = name
+        self.iconImage = iconImage
+    }
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MVCExample_2_4' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MVCExample_2_4
+  pod 'Alamofire', '~> 5.0'
+  pod 'Bond'
+  pod 'Kingfisher', '~> 5.0'
+  pod 'RealmSwift'
+
+end
