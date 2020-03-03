165df15c23dd6326d45a35aac09b70e3841982e0
laurayesayan <laurayesayan@gmail.com>
2020-03-03 22:01:12 +0300

    Initial Commit

---
+++ MVCExample_2_4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCExample_2_4
+//
+//  Created by Лаура Есаян on 03.03.2020.
+//  Copyright © 2020 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
